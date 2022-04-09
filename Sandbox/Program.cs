67d84c785695033d3b68e43f9b2a084d20c2d28a
Emrecan <emrecanyasar8@outlook.com>
2022-04-09 12:40:11 +0300

    ep1

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int yesCounter=0;
+int noCounter=0;
+Console.WriteLine($"Yes Counts:{yesCounter}");
+Console.WriteLine($"No Counts:{noCounter}");
+
+if (yesCounter>noCounter)
+{
+    Console.WriteLine($"Yes Won");
+}
+else if (noCounter > yesCounter)
+{
+    Console.WriteLine($"No Won");
+}
+else
+{
+    Console.WriteLine($"DRAW");
+}
